9110fc8269e9cb3af89297bd0b2b81193b3f36d5
Marvin Del Cid <mdc@auto-graphics.com>
2023-01-24 18:48:48 -0800

    Added 2 forms with inputs and buttons, resized header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+
+
+
 
 function App() {
+  const [albumName, setAlbumName] = useState('Enter album name...');
+  const [albumName2, setAlbumName2] = useState('Enter album name...');
+
+
+  useEffect(() => {
+    console.log('Inside useEffect()');
+    console.log(albumName);
+    console.log(albumName2);
+  })
+
+  function handleSubmit(event){
+    event.preventDefault();
+    alert(`This is your album name: ${albumName}`)
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,17 +29,26 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main className="Main">
+        <br />
+        <form onSubmit={handleSubmit}>
+          <div className="Form">
+            <label htmlFor="albumNameInput">Album Name: </label>
+            <input type="text" id="albumNameInput" value={albumName} onChange={(e) => setAlbumName(e.target.value)} />
+            <button type="submit">Enter</button>
+          </div>
+        </form>
+        <form onSubmit={handleSubmit}>
+          <div className="Form">
+            <label htmlFor="albumName2Input">Album Name 2 :</label>
+            <input type="text" id="albumName2Input" value={albumName2} onChange={(e) => setAlbumName2(e.target.value)} />
+            <button type="submit">Enter</button>
+          </div>
+        </form>
+      </main>
     </div>
-  );
+      );
 }
 
 export default App;
